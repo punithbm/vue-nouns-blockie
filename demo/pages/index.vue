c2b9f2c78c5de7e1de296e144bd2325bd4d573e4
Punith B M <punith@designstring.com>
2024-02-26 13:37:38 +0530

    feat: read me update

--- demo/pages/index.vue
+++ demo/pages/index.vue
@@ -1,11 +1,28 @@
+<script setup lang="ts">
+import { NounsBlockie } from 'vue-nouns-blockie'
+</script>
+
 <template>
-  <Tutorial />
+  <div class="container">
+    <!-- <input v-model="inputText" placeholder="Enter text or web3 address" class="input-field" @input="updateAvatar" /> -->
+    <div>
+      <NounsBlockie input="My Demo Text" ></NounsBlockie>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
-  name: 'IndexPage',
-})
-</script>
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+}
+.input-field {
+  margin-bottom: 20px;
+  padding: 8px;
+  width: 300px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+</style>
